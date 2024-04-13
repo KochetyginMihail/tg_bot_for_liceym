2229e0d6d98f33db410d27adb55f0437f218d949
KochetyginMihail <kochetygin.m@sch2009.net>
2024-04-13 23:14:19 +0300

    prototypies of numbers

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ import os
 import telebot
 import sqlite3
 import aspose.words as aw
+import random
 
 TOKEN = '7041510449:AAECwA7tOmc43RcpLbWXl-zlKrpOkZUnI20'
 bot = telebot.TeleBot(TOKEN)
 
-# Глобальная переменная для отслеживания текущего состояния развития диалога
+# Глобальные переменные для отслеживания текущего состояния развития диалога
 current_state = None
 current_task_number = None
-current_task_theory = None
+current_task_difficulty = None
+current_task_prototype = None
 
 # Подключение к базе данных
 con = sqlite3.connect("sql_bd.db", check_same_thread=False)
 cur = con.cursor()
+
+# Словарь для соответствия русских и английских названий сложности
+difficulty_mapping = {
+    "Лёгкие": "лёгкое",
+    "Нормальные": "нормальное",
+    "Сложные": "сложное",
+    "Все сразу": "%"
+}
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -27,7 +37,7 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    global current_state
+    global current_state, current_task_number
     text = message.text.lower()
 
     if text == 'теория':
@@ -53,9 +63,9 @@ def handle_message(message):
 
     elif current_state == 'практика':
         if text.isdigit() and 1 <= int(text) <= 9:
-            bot.reply_to(message, f"Здесь будут прототипы задания {text}.")
-        else:
-            bot.reply_to(message, "Прости, я не понял ваш запрос. Попробуйте ввести номер задания от 1 до 9.")
+            current_task_number = int(text)
+            bot.reply_to(message, "Прототипы успешно найдены. Выберите категорию сложности прототипов:",
+                         reply_markup=create_prototype_difficulty_keyboard())
 
     else:
         bot.reply_to(message, "Прости, я не понял ваш запрос. Попробуйте еще раз.")
@@ -70,14 +80,37 @@ def create_theory_format_keyboard():
     return keyboard
 
 
+def create_prototype_difficulty_keyboard():
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    for difficulty in difficulty_mapping.keys():
+        keyboard.add(telebot.types.InlineKeyboardButton(text=difficulty, callback_data=difficulty))
+    return keyboard
+
+
+def get_random_prototype(difficulty):
+    content = cur.execute("""SELECT * FROM practice WHERE id = ? AND exercise_type LIKE ?""",
+                          (current_task_number, difficulty_mapping[difficulty])).fetchall()
+    return random.choice(content)
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback_query(call):
+    global current_task_difficulty, current_task_prototype
     if call.data == "text_message":
         bot.send_message(call.message.chat.id, current_task_theory)
     elif call.data == "word_file":
         create_and_send_word_file(call.message.chat.id, current_task_theory, current_task_number)
     elif call.data == "text_file":
         create_and_send_text_file(call.message.chat.id, current_task_theory, current_task_number)
+    elif call.data in difficulty_mapping.keys():
+        current_task_difficulty = call.data
+        prototype = get_random_prototype(current_task_difficulty)
+        current_task_prototype = prototype[1]  # Выбираем условие задачи
+        bot.send_message(call.message.chat.id, f"Вы выбрали категорию сложности '{current_task_difficulty}'. "
+                                               f"Вот прототип задания:\n\n{current_task_prototype}\n\n"
+                                               "Введите свой ответ в формате 'Ответ: 123.'.")
+    else:
+        bot.reply_to(call.message, "Прости, я не понял ваш запрос. Попробуйте еще раз.")
 
 
 def create_and_send_word_file(chat_id, content, task_number):
